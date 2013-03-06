80864a0f38a01f2bb244b72f9f77a59168f8ce99
Morgante Pell <morgante.pell@morgante.net>
2013-03-06 20:49:06 +0400

    true config

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php if ( !defined( 'HABARI_PATH' ) ) { die( 'No direct access' ); }
 Config::set( 'db_connection', array(
-	'connection_string'=>'mysql:host=localhost;dbname=gazelle_test',
-	'username'=>'root',
-	'password'=>'root',
+	'connection_string'=>'mysql:host=us-cdbr-east-03.cleardb.com;dbname=heroku_cf1b0e9ad4a94c3',
+	'username'=>'b25faa768feb4e',
+	'password'=>'f3115106',
 	'prefix'=>'habari__'
 ));
 
